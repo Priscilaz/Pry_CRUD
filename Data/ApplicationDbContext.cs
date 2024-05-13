4d325bbe1e1c9505675c43df1a9bd99bf4162bae
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-05-12 23:54:36 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pry_CRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        //Agregar los modelos aqui
+        //Cada modelo corresponde a una tabla en la BD
+    }
+}
